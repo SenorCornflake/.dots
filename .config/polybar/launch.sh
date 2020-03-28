4c3791dbd2ccc3f4c0185dcdd4e77723676d6765
Abu <cmdwannabe@gmail.com>
2020-03-28 22:37:25 +0200

    updated all

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
- 
+
 # Terminate already running bar instances
 killall -q polybar
- 
+
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
- 
-# Launch bar
-polybar bar &
- 
-echo "Bar launched"
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+polybar -c ~/.config/polybar/config.ini top &
+polybar -c ~/.config/polybar/config.ini bottom &
