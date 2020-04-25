01783ce1dd3d8e89388120d84b56bd98a9109ff6
Abu <cmdwannabe@gmail.com>
2020-04-25 17:55:16 +0200

    Changed polybar look, now using two bars

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,5 +7,5 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch
-polybar -c ~/.config/polybar/config.ini top &
-#polybar -c ~/.config/polybar/config.ini bottom &
+polybar -c ~/.config/polybar/config2.ini top &
+polybar -c ~/.config/polybar/config2.ini bottom &
