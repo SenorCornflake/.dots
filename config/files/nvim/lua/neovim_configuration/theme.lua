2527d7dce8f594de7fe0c9d7401613f228603e0f
Baker <cmdwannabe@gmail.com>
2021-12-07 07:04:44 +0200

    Move everything into LOAD_THEME

--- config/files/nvim/lua/neovim_configuration/theme.lua
+++ config/files/nvim/lua/neovim_configuration/theme.lua
@@ -1,20 +1,20 @@
 local util = require "neovim_configuration.util"
 local cmd = vim.cmd
 
-local colorscheme = io.open(os.getenv("XDG_DATA_HOME") .. "/nvim/colorscheme.txt", "r")
-
-print(colorscheme)
-if colorscheme == nil then
-	colorscheme = "darkblue"
-else
-	colorscheme = colorscheme:read()
-end
-
-local config_dir = os.getenv("XDG_CONFIG_HOME")
-local has_theme = util.file_exists(config_dir .. "/nvim/lua/neovim_configuration/themes/" .. colorscheme .. ".lua")
-
 -- All the doautocmds here are from before I used nix, it was for lazy loading, I don't lazy load anymore because of some issues I had but I think I'll leave these doautocmds here for future reference
 LOAD_THEME = function()
+	local colorscheme = io.open(os.getenv("XDG_DATA_HOME") .. "/nvim/colorscheme.txt", "r")
+
+	print(colorscheme)
+	if colorscheme == nil then
+		colorscheme = "darkblue"
+	else
+		colorscheme = colorscheme:read()
+	end
+
+	local config_dir = os.getenv("XDG_CONFIG_HOME")
+	local has_theme = util.file_exists(config_dir .. "/nvim/lua/neovim_configuration/themes/" .. colorscheme .. ".lua")
+
 	if not has_theme then
 		cmd ("doautocmd User load_" .. colorscheme .. "_theme")
 		cmd ("colorscheme " .. colorscheme)
